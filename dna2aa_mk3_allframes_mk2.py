bbe6e8496bcaaa4ffba1c062ecae25ff4e71b351
Tan Kwan Ann <36640032+Baboolal@users.noreply.github.com>
2021-06-29 17:37:40 +0800

    Add files via upload

--- dna2aa_mk3_allframes_mk2.py
+++ dna2aa_mk3_allframes_mk2.py
@@ -5,55 +5,19 @@ from Bio import SeqIO
 from Bio.Seq import Seq, translate
 from Bio.SeqRecord import SeqRecord
 
-# colour printer
-def colour_printer(aa_seq):
-    M_state = False
-    for letter in aa_seq:
-        if letter == "M":
-            M_state = True
-        elif letter == "*":
-            M_state = False
-
-        if M_state and (letter != "C"):
-            print("\033[0;30;41m{}".format(letter), end="") # print highlighted
-        elif M_state and (letter == "C"):
-            print("\033[0;30;44m{}".format(letter), end="") # print a cysteine
-        elif letter == "C":
-            print("\033[0;30;44m{}".format(letter), end="") # print a cysteine
-        else:
-            #print("\033[0;37;40m{}".format(letter), end="") # print normal
-            print("\033[0m{}".format(letter), end="") # print normal
-    
-    print("\033[0m") # newline
-
 # storage of AA sequence in list (declaration)
 aa_seq_data = [[],[],[],[],[],[]]
 
 # iterate and convert DNA to AA
-for row in SeqIO.parse(r"HC-1A-Trinity.fa", "fasta"):
+for row in SeqIO.parse(r"seqdump_caripe1.txt", "fasta"):
     frame_n_seq = [] # declare
-    print(row.id)
+    #print(row.id)
     frame_n_seq.append(translate(str(row.seq))) # frame 1
     frame_n_seq.append(translate(str(row.seq)[1:])) # frame 2
     frame_n_seq.append(translate(str(row.seq)[2:])) # frame 3
     frame_n_seq.append(translate(str(row.seq)[::-1][2:])) # frame -3
     frame_n_seq.append(translate(str(row.seq)[::-1][1:])) # frame -2
     frame_n_seq.append(translate(str(row.seq)[::-1])) # frame -1
-    print("Frame 1: ", end="") # replace all these prints with colour printer
-    colour_printer(frame_n_seq[0])
-    print("Frame 2: ", end="")
-    colour_printer(frame_n_seq[1])
-    print("Frame 3: ", end="")
-    colour_printer(frame_n_seq[2])
-    print("Frame -1: ", end="")
-    colour_printer(frame_n_seq[-1])
-    print("Frame -2: ", end="")
-    colour_printer(frame_n_seq[-2])
-    print("Frame -3: ", end="")
-    colour_printer(frame_n_seq[-3])
-
-    # print some lines
-    print("----------------------------------------------")
 
     # save data of all frames
     frames = [0, 1, 2, -1, -2, -3]
@@ -67,9 +31,9 @@ for row in SeqIO.parse(r"HC-1A-Trinity.fa", "fasta"):
         aa_seq_data[k].append(frame_record)
 
 # write all files here
-SeqIO.write(aa_seq_data[0], "HC-1A-Trinity_aa_frame1.fa", "fasta")
-SeqIO.write(aa_seq_data[1], "HC-1A-Trinity_aa_frame2.fa", "fasta")
-SeqIO.write(aa_seq_data[2], "HC-1A-Trinity_aa_frame3.fa", "fasta")
-SeqIO.write(aa_seq_data[-1], "HC-1A-Trinity_aa_frame-1.fa", "fasta")
-SeqIO.write(aa_seq_data[-2], "HC-1A-Trinity_aa_frame-2.fa", "fasta")
-SeqIO.write(aa_seq_data[-3], "HC-1A-Trinity_aa_frame-3.fa", "fasta")
+SeqIO.write(aa_seq_data[0], "seqdump_caripe1_aa_frame1.fa", "fasta")
+SeqIO.write(aa_seq_data[1], "seqdump_caripe1_aa_frame2.fa", "fasta")
+SeqIO.write(aa_seq_data[2], "seqdump_caripe1_aa_frame3.fa", "fasta")
+SeqIO.write(aa_seq_data[-1], "seqdump_caripe1_aa_frame-1.fa", "fasta")
+SeqIO.write(aa_seq_data[-2], "seqdump_caripe1_aa_frame-2.fa", "fasta")
+SeqIO.write(aa_seq_data[-3], "seqdump_caripe1_aa_frame-3.fa", "fasta")
