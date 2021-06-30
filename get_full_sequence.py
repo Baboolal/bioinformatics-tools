554c02b8262c44db970b45a48dd550d863f36bfe
Tan Kwan Ann <36640032+Baboolal@users.noreply.github.com>
2021-06-30 10:39:36 +0800

    Add files via upload

--- get_full_sequence.py
+++ get_full_sequence.py
@@ -1,5 +1,6 @@
 # retrieve full sequence of protein
-# input: fasta file of short sequences
+# input1: fasta file of short sequences (motifed)
+# input2: fasta file of full sequences
 # output1: tab-delimited file of: id <tab> short seq <tab> full seq
 # output2: fasta file of the full sequences
 
@@ -7,14 +8,14 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 
 # read in the full sequences file into a dict (memory efficient)
-full_record_dict = SeqIO.index("HC-1A-Trinity_aa_frame3.fa", "fasta")
+full_record_dict = SeqIO.index("seqdump_caripe1_aa_frame3.fa", "fasta")
 
 # prep tab-delimited txt file to write
-output_file = open("HC_cyclo_frame3_table.txt", "w")
+output_file = open("seqdump_caripe1_cyclo_frame3_table.txt", "w")
 fasta_file_write_entries = []
 
 # real deal
-for record in SeqIO.parse("HC_cyclo_frame3.fa", "fasta"):
+for record in SeqIO.parse("seqdump_caripe1_cyclo_frame2.fa", "fasta"):
     print(record.id, record.seq, full_record_dict[record.id.split("/")[0]].seq)
     output_file.write(str(record.id) + "\t" + str(record.seq) \
         + "\t" + str(full_record_dict[record.id.split("/")[0]].seq) + "\n")
@@ -25,4 +26,4 @@ for record in SeqIO.parse("HC_cyclo_frame3.fa", "fasta"):
     fasta_file_write_entries.append(fasta_entry)
 
 output_file.close()
-SeqIO.write(fasta_file_write_entries, "HC_cyclo_frame3_full.fa", "fasta")
+SeqIO.write(fasta_file_write_entries, "seqdump_caripe1_cyclo_frame3_full.fa", "fasta")
